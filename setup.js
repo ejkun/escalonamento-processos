d5ff80fdd0a652845dd6d6f7ead9604da6ca22fe
Everton Barbosa Jr <everton.barbosa@uft.edu.br>
2017-11-29 16:31:14 -0300

    Melhora funções process e push
    Agora elas são atreladas as listas e não há necessidade de dizer qual lista deve ser usada.
    Existe também um fator de probabilidade, que é o bônus, permitindo que um processo seja penalizado ou não

--- setup.js
+++ setup.js
@@ -1,9 +1,22 @@
+function bonus() {
+  	var valores = [-1, -1, 0, 0, 0, 0, 0, 0, 1, 1];
+  	var indice = Math.floor(Math.random() * valores.length);
+	return valores[indice];
+}
+
 jQuery.fn.extend({
 	textQuantum: function (quantum) {
 		$(this).text("- Quantum "+quantum);
 		$(this).data('val', quantum);
 	}
 });
+
+labels = {
+	1: "danger",
+	2: "warning",
+	3: "success",
+	4: "info"
+};
 
 var quantumProcessador = 2;
 
@@ -13,28 +26,62 @@ for (i = 1; i <= 4; i++) {
 	listas[i] = {
 		arr: new ObservableRoundRobin(),
 		ul: $("#lista-"+i),
-		quantum: $("#quantum-l"+i)
+		quantumElement: $("#quantum-l"+i),
+		length: function () {
+			return this.arr.length;
+		},
+		shift: function () {
+			return this.arr.shift();
+		},
+		quantum: function () {
+			return this.quantumElement.data('val');
+		}
 	};
 
-	listas[i].quantum.textQuantum(Math.floor(Math.random() * 5) + i*4);
+	listas[i].quantumElement.textQuantum(Math.floor(Math.random() * 5) + i*4);
 
 	listas[i].arr.addEventListener("itemadded", function (e) {
-		listas[e.item.list].ul.append("<li class='list-group-item list-group-item-danger'>"+ e.item.name + " - Q:" + e.item.quantum +"</li>");
+		listas[e.item.fila].ul.append("<li class='list-group-item list-group-item-"+labels[e.item.fila]+"'>"+ e.item.name + " - Q:" + e.item.quantum +"</li>");
 	});
 
 	listas[i].arr.addEventListener("itemremoved", function (e) {
-		listas[e.item.list].ul.children().eq(e.index).remove();
+		listas[e.item.fila].ul.children().eq(e.index).remove();
 		if (e.item.quantum > quantumProcessador) {
 			e.item.quantum -= quantumProcessador;
-			listas[e.item.list].arr.push(e.item);
+			listas.push(e.item, bonus());
 		}
 	});
 }
 
-function push(object) {
-	return listas[object.list].arr.push(object)
+listas.process = function () {
+	for (i = 1; i <= 4; i++) {
+		if (this[i].length() > 0) {
+			return this[i].shift();
+		}
+	}
+	return false;
+};
+
+listas.push = function (processo, bonus) {
+	if (typeof bonus === 'undefined') { bonus = 0; }
+	for (i = 1; i <= 3; i++) {
+		if (processo.quantum <= this[i].quantum()) {
+			processo.fila = i - bonus;
+			if (bonus === -1) {
+				console.log(processo.name + " foi gratificado");
+			}
+			if (bonus === 1) {
+				console.log(processo.name + " foi penalizado");
+			}
+			this[processo.fila].arr.push(processo);
+			return true;
+		}
+	}
+	processo.fila = i;
+	this[i].arr.push(processo);
+	return true;
 }
 
-function shift(list) {
-	return listas[list].arr.shift();
+function generateQuantum() {
+	return Math.floor(Math.random() * 20) + 1;
 }
